dbe9674d8b8c51e674b6101268885999899a5fdf
gotoxy <tommasocolella95@gmail.com>
2021-05-27 18:06:36 +0200

    add cast_envelope fixes, formatted code

--- contracts/Mayor.sol
+++ contracts/Mayor.sol
@@ -2,62 +2,76 @@
 pragma solidity 0.8.4;
 
 contract Mayor {
-    
     // Structs, events, and modifiers
-    
+
     // Store refund data
     struct Refund {
-        uint soul;
+        uint256 soul;
         bool doblon;
     }
 
     struct Candidate {
-        uint soul;
-        uint votes;
+        uint256 soul;
+        uint256 votes;
     }
-    
+
     // Data to manage the confirmation
     struct Conditions {
         uint32 quorum;
         uint32 envelopes_casted;
         uint32 envelopes_opened;
         bool outcome_declared;
-        bool cadidates_finished_depositing;
+        uint256 candidates_deposits_left;
     }
-    
+
     event NewMayor(address _candidate);
     event Sayonara(address _escrow);
     event RefundedVoter(address _voter);
     event EnvelopeCast(address _voter);
-    event EnvelopeOpen(address _voter, uint _soul, bool _doblon);
-    event VoteForMe(address _candidate, uint _soul);
-    
+    event EnvelopeOpen(address _voter, uint256 _soul, bool _doblon);
+    event VoteForMe(address _candidate, uint256 _soul);
+
     // Someone can vote as long as the quorum is not reached and the candidates finished depositing their soul
     modifier canVote() {
-        require(voting_condition.outcome_declared, "You cannot vote until all candidates have finalised their participation!");
-        require(voting_condition.envelopes_casted < voting_condition.quorum, "Cannot vote now, voting quorum has been reached");
-        _;   
+        require(
+            voting_condition.candidates_deposits_left == 0,
+            "You cannot vote until all candidates have finalised their participation!"
+        );
+        require(
+            voting_condition.envelopes_casted < voting_condition.quorum,
+            "Cannot vote now, voting quorum has been reached"
+        );
+        _;
     }
-    
+
     // Envelopes can be opened only after receiving the quorum
     modifier canOpen() {
-        require(voting_condition.envelopes_casted == voting_condition.quorum, "Cannot open an envelope, voting quorum not reached yet");
+        require(
+            voting_condition.envelopes_casted == voting_condition.quorum,
+            "Cannot open an envelope, voting quorum not reached yet"
+        );
         _;
     }
-    
+
     // The outcome of the confirmation can be computed as soon as all the casted envelopes have been opened
     modifier canCheckOutcome() {
-        require(voting_condition.envelopes_opened == voting_condition.quorum, "Cannot check the winner, need to open all the sent envelopes");
-        require(voting_condition.outcome_declared == false, "The winner has already been declared!");
+        require(
+            voting_condition.envelopes_opened == voting_condition.quorum,
+            "Cannot check the winner, need to open all the sent envelopes"
+        );
+        require(
+            voting_condition.outcome_declared == false,
+            "The winner has already been declared!"
+        );
         _;
     }
-    
+
     // State attributes
-    
+
     // Initialization variables
     address[] public candidates;
     address payable public escrow;
-    
+
     // Candidates mapping for soul depositing and counting votes
     mapping(address => bool) is_candidate;
     mapping(address => Candidate) candidate_standings;
@@ -67,8 +81,8 @@ contract Mayor {
 
     Conditions voting_condition;
 
-    uint public naySoul;
-    uint public yaySoul;
+    uint256 public naySoul;
+    uint256 public yaySoul;
 
     // Refund phase variables
     mapping(address => Refund) souls;
@@ -78,79 +92,107 @@ contract Mayor {
     /// @param _candidates (address) The address of the mayor candidate
     /// @param _escrow (address) The address of the escrow account
     /// @param _quorum (address) The number of voters required to finalize the confirmation
-    constructor(address[] memory _candidates, address payable _escrow, uint32 _quorum) public {
+    constructor(
+        address[] memory _candidates,
+        address payable _escrow,
+        uint32 _quorum
+    ) public {
+        require((_candidates.length > 1), "You need two candidates at least!");
+
         // add to mapping to check if they're really candidates
-        for(uint i = 0; i < _candidates.length; i++) {
+        for (uint256 i = 0; i < _candidates.length; i++) {
             candidates.push(_candidates[i]);
             is_candidate[candidates[i]] = true;
         }
 
         escrow = _escrow;
-        voting_condition = Conditions({quorum: _quorum, envelopes_casted: 0, envelopes_opened: 0, outcome_declared: false, cadidates_finished_depositing: false});
+        voting_condition = Conditions({
+            quorum: _quorum,
+            envelopes_casted: 0,
+            envelopes_opened: 0,
+            outcome_declared: false,
+            candidates_deposits_left: _candidates.length
+        });
     }
 
     /// @notice Let candidates store their soul for cookies ;)
     /// soul can only deposited once, the sender must be a candidate, of course.
-    function deposit_soul () public payable {
-        require(is_candidate[msg.sender], "You're not a candidate in this election!");
-        require(candidate_standings[msg.sender].soul == 0, "You've already deposited some soul!");
+    function deposit_soul() public payable {
+        require(
+            is_candidate[msg.sender],
+            "You're not a candidate in this election!"
+        );
+        require(
+            candidate_standings[msg.sender].soul == 0,
+            "You've already deposited some soul!"
+        );
 
         // add the candidate to the standings
-        candidate_standings[msg.sender] = Candidate({soul: msg.value, votes: 0});
-        
+        candidate_standings[msg.sender] = Candidate({
+            soul: msg.value,
+            votes: 0
+        });
+
+        // decrease the needed deposits
+        voting_condition.candidates_deposits_left--;
         // just to annouce we've got free cookies :)
         emit VoteForMe(msg.sender, msg.value);
-
     }
 
     /// @notice Store a received voting envelope
-    /// @param _envelope The envelope represented as the keccak256 hash of (sigil, doblon, soul) 
-    function cast_envelope(bytes32 _envelope) canVote public {
-        
-        if(envelopes[msg.sender] == 0x0) // => NEW, update on 17/05/2021
+    /// @param _envelope The envelope represented as the keccak256 hash of (sigil, doblon, soul)
+    function cast_envelope(bytes32 _envelope) public canVote {
+        if (envelopes[msg.sender] == 0x0)
+            // => NEW, update on 17/05/2021
             voting_condition.envelopes_casted++;
 
         envelopes[msg.sender] = _envelope;
         emit EnvelopeCast(msg.sender);
-
     }
-    
-    
+
     /// @notice Open an envelope and store the vote information
     /// @param _sigil (uint) The secret sigil of a voter
     /// @param _doblon (bool) The voting preference
     /// @dev The soul is sent as crypto
     /// @dev Need to recompute the hash to validate the envelope previously casted
-    function open_envelope(uint _sigil, bool _doblon) canOpen public payable {
-        
-        require(envelopes[msg.sender] != 0x0, "The sender has not casted any votes");
-        
-        bytes32 _casted_envelope = envelopes[msg.sender];
-        bytes32 _sent_envelope = keccak256(abi.encode(_sigil, _doblon, msg.value));
+    function open_envelope(uint256 _sigil, bool _doblon)
+        public
+        payable
+        canOpen
+    {
+        require(
+            envelopes[msg.sender] != 0x0,
+            "The sender has not casted any votes"
+        );
 
-        require(_casted_envelope == _sent_envelope, "Sent envelope does not correspond to the one casted");
-        require(souls[msg.sender].soul == 0, "You've already opened your envelope!");
+        bytes32 _casted_envelope = envelopes[msg.sender];
+        bytes32 _sent_envelope =
+            keccak256(abi.encode(_sigil, _doblon, msg.value));
+
+        require(
+            _casted_envelope == _sent_envelope,
+            "Sent envelope does not correspond to the one casted"
+        );
+        require(
+            souls[msg.sender].soul == 0,
+            "You've already opened your envelope!"
+        );
 
         // let's prepare for a possible refund
         souls[msg.sender] = Refund(msg.value, _doblon);
         voters.push(msg.sender);
-        
-        if(_doblon == true)
-            yaySoul += msg.value;
-        else
-            naySoul += msg.value;
-            
+
+        if (_doblon == true) yaySoul += msg.value;
+        else naySoul += msg.value;
+
         // we increase the envelopes opened (we should also check for already opened envelope)
         voting_condition.envelopes_opened++;
-        
 
         emit EnvelopeOpen(msg.sender, msg.value, _doblon);
     }
-    
-    
-    /// @notice Either confirm or kick out the candidate. Refund the electors who voted for the losing outcome
-    function mayor_or_sayonara() canCheckOutcome public {
 
+    /// @notice Either confirm or kick out the candidate. Refund the electors who voted for the losing outcome
+    function mayor_or_sayonara() public canCheckOutcome {
         // in order not to exploit this multiple times
         voting_condition.outcome_declared = true;
 
@@ -162,7 +204,6 @@ contract Mayor {
             // candidate.transfer(yaySoul);
             // emit NewMayor(candidate);
         }
-
         // sayonara! pay the escrow
         else {
             escrow.transfer(naySoul);
@@ -170,26 +211,26 @@ contract Mayor {
         }
 
         // refund losing voters
-        for (uint i = 0; i < voters.length; i++){
-            // if the voter "won", no refund 
-            if (souls[voters[i]].doblon == confirmed)
-                continue;
+        for (uint256 i = 0; i < voters.length; i++) {
+            // if the voter "won", no refund
+            if (souls[voters[i]].doblon == confirmed) continue;
             else {
                 address payable to_refund = payable(voters[i]);
                 to_refund.transfer(souls[to_refund].soul);
                 emit RefundedVoter(to_refund);
             }
         }
-
     }
- 
- 
+
     /// @notice Compute a voting envelope
     /// @param _sigil (uint) The secret sigil of a voter
     /// @param _symbol (addres) The symbol of a candidate, represented by his address
     /// @param _soul (uint) The soul associated to the vote
-    function compute_envelope(uint _sigil, address _symbol, uint _soul) public pure returns(bytes32) {
+    function compute_envelope(
+        uint256 _sigil,
+        address _symbol,
+        uint256 _soul
+    ) public pure returns (bytes32) {
         return keccak256(abi.encode(_sigil, _symbol, _soul));
     }
-    
 }
--- test/mayor_generic_test.js
+++ test/mayor_generic_test.js
@@ -8,19 +8,19 @@ instance = null;
 contract("Mayor, generic tests", async accounts => {
     it("Should test the constructor", async () => {
         // account 0 creates a new election with three candidates, a quorum of 4
-        instance = await Mayor.new([accounts[0], accounts[1], accounts[2]], accounts[3], 4, {from: accounts[0]});
+        instance = await Mayor.new([accounts[0], accounts[1], accounts[2]], accounts[3], 4, { from: accounts[0] });
         return (instance == true);
-        
+
     });
 
     it("Should test soul depositing from the candidates", async () => {
-        
+
         let counter = 0;
-        for (i = 0; i < 3; i++){
+        for (i = 0; i < 3; i++) {
             // if we successfully deposit some soul
-           deposit = await instance.deposit_soul({from: accounts[i], value: 100});
-           if (deposit)
-            counter += 1;
+            deposit = await instance.deposit_soul({ from: accounts[i], value: 100 });
+            if (deposit)
+                counter += 1;
         }
 
         return (counter == 3);
@@ -29,23 +29,23 @@ contract("Mayor, generic tests", async accounts => {
 
     it("Should test failure when double depositing", async () => {
         // another instance just for this test
-        _instance = await Mayor.new([accounts[0], accounts[1]], accounts[2], 3, {from: accounts[0]})
+        _instance = await Mayor.new([accounts[0], accounts[1]], accounts[2], 3, { from: accounts[0] })
         // this will go smoothly
-        deposit = await _instance.deposit_soul({from: accounts[0], value: 100});
+        deposit = await _instance.deposit_soul({ from: accounts[0], value: 100 });
         // this will fail
         await truffleAssert.fails(
-            _instance.deposit_soul({from: accounts[0], value: 100}),
+            _instance.deposit_soul({ from: accounts[0], value: 100 }),
         );
 
     });
 
     it("Should test failure when the deposit doesn't come from a candidate", async () => {
         // another instance just for this test
-        _instance = await Mayor.new([accounts[0], accounts[1]], accounts[2], 3, {from: accounts[0]})
+        _instance = await Mayor.new([accounts[0], accounts[1]], accounts[2], 3, { from: accounts[0] })
 
         // this will fail
         await truffleAssert.fails(
-            _instance.deposit_soul({from: accounts[9], value: 100}),
+            _instance.deposit_soul({ from: accounts[9], value: 100 }),
         );
 
     });
@@ -55,26 +55,51 @@ contract("Mayor, generic tests", async accounts => {
         // precompute the envelope
         let _envelope = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(["uint", "address", "uint"], [1, accounts[0], 1]));
         // check if returned envelope is the same as the one we computed before
-        return instance.compute_envelope(1, accounts[0], 1, {from: accounts[2]})
-        .then(envelope => assert.equal(envelope, _envelope));
-        
+        return instance.compute_envelope(1, accounts[0], 1, { from: accounts[2] })
+            .then(envelope => assert.equal(envelope, _envelope));
+
+    });
+
+    it("Should test cast_envelope", async () => {
+        // another instance just for this test
+        _instance = await Mayor.new([accounts[0], accounts[1]], accounts[2], 3, { from: accounts[0] });
+
+        _instance.deposit_soul({ from: accounts[0], value: 100 });
+        _instance.deposit_soul({ from: accounts[1], value: 100 });
+
+        let envelope = await _instance.compute_envelope(1, accounts[0], 1, { from: accounts[9] });
+        console.log(envelope);
+        return _instance.cast_envelope(envelope, { from: accounts[9] });
+
+    });
+
+    it("Should test failure when casting a vote without waiting for all deposits", async () => {
+        // another instance just for this test
+        _instance = await Mayor.new([accounts[0], accounts[1]], accounts[2], 3, { from: accounts[0] })
+
+        // this will fail
+        let envelope = await instance.compute_envelope(1, accounts[0], 1, { from: accounts[9] });
+        await truffleAssert.fails(
+            _instance.cast_envelope(envelope, { from: accounts[9], value: 1 }),
+        );
+
     });
 
     it("Should correctly cast envelopes", async () => {
-        let envelope = await instance.compute_envelope(1, true, 1, {from: accounts[2]});
+        let envelope = await instance.compute_envelope(1, true, 1, { from: accounts[2] });
 
         // console.log(envelope);
-        
-        return instance.cast_envelope(envelope, {from: accounts[2]});
+
+        return instance.cast_envelope(envelope, { from: accounts[2] });
 
     });
 
     it("Should correctly open envelopes", async () => {
-        return instance.open_envelope(1, true, {from: accounts[2], value: 1})        
+        return instance.open_envelope(1, true, { from: accounts[2], value: 1 })
     });
 
-    it("Should correctly declare the winner", async() => {
-        return instance.mayor_or_sayonara({from: accounts[0]});
+    it("Should correctly declare the winner", async () => {
+        return instance.mayor_or_sayonara({ from: accounts[0] });
     });
 
 })
