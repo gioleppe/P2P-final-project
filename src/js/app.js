e24c9bd50abcfd70a29c87f95159c8c238d27bbd
gotoxy <tommasocolella95@gmail.com>
2021-07-18 08:19:27 +0200

    initial bootstrap add

--- src/js/app.js
+++ src/js/app.js
@@ -3,24 +3,25 @@ App = {
     web3Provider: null, // Web3 provider
     url: 'http://localhost:8545', // Url for web3
     account: '0x0', // current ethereum account
+    accounts: null,
     init: function () { return App.initWeb3(); },
 
     initWeb3: function () {
-        if (typeof web3 != 'undefined') { // Check whether exists a provider, e.g Metamask
-            App.web3Provider = window.ethereum; // standard since 2/11/18
-            web3 = new Web3(App.web3Provider);
-            try { // Permission popup
-                ethereum.enable().then(async () => { console.log("DApp connected"); });
-            }
-            catch (error) { console.log(error); }
-        } else { // Otherwise, create a new local instance of Web3
-            App.web3Provider = new Web3.providers.HttpProvider(App.url); // <==
-            web3 = new Web3(App.web3Provider);
-        }
+        // if (typeof web3 != 'undefined') { // Check whether exists a provider, e.g Metamask
+        //     App.web3Provider = window.ethereum; // standard since 2/11/18
+        //     web3 = new Web3(App.web3Provider);
+        //     try { // Permission popup
+        //         ethereum.enable().then(async () => { console.log("DApp connected"); });
+        //     }
+        //     catch (error) { console.log(error); }
+        // } else { // Otherwise, create a new local instance of Web3
+        App.web3Provider = new Web3.providers.HttpProvider(App.url); // <==
+        web3 = new Web3(App.web3Provider);
+        // }
         return App.initContract();
     },
 
-    initContract: function () {
+    initContract: async function () {
         // Store ETH current account
         web3.eth.getCoinbase(function (err, account) {
             if (err == null) {
@@ -35,18 +36,15 @@ App = {
             App.contracts["Mayor"].setProvider(App.web3Provider);
             return App.listenForEvents();
         });
+        // get the accounts from web3
+        App.accounts = await web3.eth.getAccounts();
+        console.log(App.accounts);
     },
 
     listenForEvents: function () {
         App.contracts["Mayor"].deployed().then(async (instance) => {
-            console.log(await instance.escrow());
-            console.log(await instance.candidates(0));
-            console.log(await instance.candidates(1));
-            console.log(await instance.candidates(2));
-        });
-        App.contracts["Mayor"].deployed().then(async (instance) => {
             // click is the Solidity event
-            instance.click().on('data', function (event) {
+            instance.VoteForMe().on('data', function (event) {
                 $("#eventId").html("Event catched!");
                 console.log("Event catched");
                 console.log(event);
@@ -60,9 +58,29 @@ App = {
         // Retrieve contract instance
         App.contracts["Mayor"].deployed().then(async (instance) => {
             // Call the value function (value is a public attribute)
-            const v = await instance.value();
-            console.log(v);
-            $("#valueId").html("" + v);
+            const escrow = await instance.escrow();
+            // console.log(await instance.candidates(0));
+            // console.log(await instance.candidates(1));
+            // console.log(await instance.candidates(2));
+
+            console.log(escrow);
+            $("#valueId").text(escrow);
+            $('#escrows').removeAttr('disabled');
+        });
+        return App.depositSoul();
+    },
+
+    depositSoul: function () {
+        App.contracts["Mayor"].deployed().then(async (instance) => {
+            counter = 0;
+            // accounts 1, 2, and 3 are the candidates
+            for (i = 1; i < 4; i++) {
+                // if we successfully deposit some soul
+                deposit = await instance.deposit_soul({ from: App.accounts[i], value: 100 });
+                if (deposit)
+                    counter += 1;
+                console.log(counter);
+            };
         });
     },
 
@@ -70,16 +88,16 @@ App = {
         App.contracts["Contract"].deployed().then(async (instance) => {
             await instance.pressClick({ from: App.account });
         });
-    }
+    },
 
 }
 
 
 
-// Call init whenever the window loads
-$(function () {
-    $(window).on('load', function () {
-        debugger;
-        App.init();
-    });
-});
+// // Call init whenever the window loads
+// $(function () {
+//     $(window).on('load', function () {
+//         debugger;
+//         App.init();
+//     });
+// });
